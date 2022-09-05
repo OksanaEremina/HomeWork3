61d0914a9688c58d0121f07845d631f6b460cff1
Oksana <ereminaoksana82@yandex.ru>
2022-09-05 13:58:17 +0400

    task19

---
+++ HW3_task19/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+// 14212 -> нет
+
+// 12821 -> да
+
+// 23432 -> да
+
+Console.WriteLine("Введите число:");
+string? line = Console.ReadLine();
+if (line != null) {
+    char[] array = line.ToCharArray();
+    for (int i = 0; i< 2; i+=1){
+        if(array[i] != array[4-i]){
+            Console.WriteLine("нет");
+            Environment.Exit(0);
+        }
+    }
+    Console.WriteLine("да");
+
+}
+
